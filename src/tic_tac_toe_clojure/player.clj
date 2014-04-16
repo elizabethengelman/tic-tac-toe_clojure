1af9e21fc057f00c97a1fa0f6fca93bf0cd0083e
Elizabeth Engelman <emengelman@gmail.com>
2014-04-16 12:51:01 -0500

    handles inputs that aren't numbers by continuing to prompty user for a move

--- src/tic_tac_toe_clojure/player.clj
+++ src/tic_tac_toe_clojure/player.clj
@@ -1,8 +1,15 @@
 (ns tic_tac_toe_clojure.player
 	(:use [tic_tac_toe_clojure.cli]))
 
+(defn is-a-number[str]
+  (let [n (read-string str)]
+       (if (number? n) n nil)))
+
 (defn ask-for-move[player-number]
 	(if (= player-number 0)
 		(print-message "Player 1, where would you like to place your X?")
 		(print-message "Player 2, where would you like to place your O?"))
-	(Integer. (get-input-from-user)))
+	(def move (get-input-from-user))
+	(if (is-a-number move)
+		(Integer. move)
+		0))
