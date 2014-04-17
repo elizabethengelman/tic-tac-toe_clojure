c773e35f273e81955018e70a10000256604d8bca
Elizabeth Engelman <emengelman@gmail.com>
2014-04-17 11:28:55 -0500

    user can choose if they want to play against a human or computer, and if playing against the computer, they can choose to go first or second

--- src/tic_tac_toe_clojure/core.clj
+++ src/tic_tac_toe_clojure/core.clj
@@ -9,37 +9,68 @@
   (print-message "Please enter who you would you like to play against: a human (type in 'human') or a dumb computer (type in 'dumb')."))
 
 (defn set-opponent[]
-  (def opponent (get-input-from-user)))
+  (if (= (get-input-from-user) "dumb")
+    (def opponent "dumb computer")
+    (def opponent "human")))
+
 
 (defn start-the-game[]
    (print-message "Welcome to Tic Tac Toe!\n")
    (print-message "Player 1 will be X's and Player 2 will be O's. This is how the board is numbered:\n")
    (print-message (create-numbered-board-for-display)))
 
-(defn game-loop[]
+(defn change-player[opponent current-player]
+    (cond 
+      (= opponent "dumb computer")
+        (if (= current-player 0)
+          2
+          0)
+      (= opponent "human")
+        (- 1 current-player)))
+
+(defn first-or-second[]
+  (print-message "Would you like to go first or second?")
+  (if (= (get-input-from-user) "first")
+    0
+    2))
+
+(defn set-starter[]
+  (if (= opponent "dumb computer")
+    (def starter (first-or-second))
+    (def starter (Integer. 0))))
+
+(defn game-loop[opponent current-player]
   (let [current-board (create-new-board)] 
       (def board   
       (loop [game "in progress"
              turn-counter 0 
-	           current-player 0 
+	           current-player current-player
              current-board current-board] 
       (if (game-over? current-board)
         current-board 
         (recur
           (print-board current-board)
           (+ turn-counter 1)
-          (- 1 current-player)
-          (update-board current-board current-player )))))
+          (change-player opponent current-player)
+          (update-board current-board current-player)))))
     (print-board board) 
     (print-message "Game over!")
     (print-message (game-outcome board))))
 
 (defn -main[]
+   (start-the-game)
    (ask-computer-or-human)
    (set-opponent)
-   (start-the-game)
-   (game-loop))
+   (set-starter)
+   (print "this is the opponent you've chosen: "opponent "\n")
+   (game-loop opponent starter))
   
+
+
+
+; human 1 : 0
+; human 2 : 1
+; dumb compter : 2
 
 ;TODO
   ;user chooses who goes first
--- src/tic_tac_toe_clojure/game.clj
+++ src/tic_tac_toe_clojure/game.clj
@@ -1,7 +1,8 @@
 (ns tic_tac_toe_clojure.game
 	(:use [tic_tac_toe_clojure.player]
         [tic_tac_toe_clojure.rules]
-        [tic_tac_toe_clojure.computer]))
+        [tic_tac_toe_clojure.computer]
+        [tic_tac_toe_clojure.cli]))
 
 (defn valid-move?[move current-board]
   (and 
@@ -10,19 +11,26 @@
 
 
 (defn get-computer-move[current-board]
+  (print-message "The computer is going...")
   (loop [computer-move (rand-int 10)]
     (if (valid-move? computer-move current-board)
       computer-move
       (recur (rand-int 10)))))
 
-(defn get-move[player-number current-board]
-  (if (= player-number 0)
+(defn get-human-move[player-number current-board]
     (loop [move (ask-for-move player-number)]
       (if (valid-move? move current-board)
         move
-        (recur(ask-for-move player-number))))
-    (get-computer-move current-board)))
+        (recur(ask-for-move player-number)))))
 
+(defn get-move[player-number current-board]
+  (cond 
+    (= player-number 0)
+      (get-human-move player-number current-board)
+    (= player-number 1)
+      (get-human-move player-number current-board)
+    (= player-number 2)
+      (get-computer-move current-board)))
 
 (defn get-mark[player-number]
   (if (= player-number 0)
