148f42f39034cd77398c851b0451e12e50e51d5a
Elizabeth Engelman <emengelman@gmail.com>
2014-04-21 13:15:04 -0500

    refactored tests and implementation of setup

--- spec/tic_tac_toe_clojure/setup_spec.clj
+++ spec/tic_tac_toe_clojure/setup_spec.clj
@@ -14,22 +14,22 @@
 
 	(describe "ask-for-opponent"
 		(it "asks if the user would like to play against the computer or another human"
-			(should= "Please enter who you would you like to play against: a human (type in 'human') or a dumb computer (type in 'dumb').\n"
+			(should= "Please enter who you would you like to play against: a human (type in 'human') or a dumb computer (type in 'dumb computer').\n"
 				(with-out-str
 					(ask-for-opponent)))))
 
 	(describe "get-opponent"
 		(it "gets which opponent the user wants to play against"
 		  (should= "dumb computer"
-		  	(with-in-str "dumb"
+		  	(with-in-str "dumb computer"
 			(get-opponent)))))
 
-    (describe "first-or-second"
+    (describe "ask-first-or-second"
     	(it "asks the user if they would like to go first or second"
     		(should= "Would you like to go first or second?\n"
     			(with-out-str
     				(ask-first-or-second)))))
-
+    (describe "get-first-or-second-input"
     	(it "returns the player-number 0 if the user wants to go first"
     		(should= 0
     			(with-in-str "first"
@@ -38,10 +38,21 @@
     	(it "returns the player-number 2 if the user wants to go second"
     		(should= 2
     			(with-in-str "second"
-    				(get-first-or-second-input))))
+    				(get-first-or-second-input)))))
 
+    (describe "set-starter"
 
-    	(describe "starter"
-    		(it "sets the starting player, depending on user inputer")))
+        (it "returns 0 if the user is playing again another human"
+            (should= 0 (set-starter "human")))
+
+    	(it "returns 0, if the user wants to go first vs. the computer"
+            (should= 0
+                (with-in-str "first"
+                (set-starter "dumb computer")))))
+
+        (it "returns 2 if the user wants to go second vs. the computer"
+            (should= 2
+                (with-in-str "second"
+                (set-starter "dumb computer")))))
 
 
